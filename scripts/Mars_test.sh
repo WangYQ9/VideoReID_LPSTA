9a2a9c869b65c1e588cda8699e50e45027b5d9aa
WangYQ9 <2211908007@stmail.ujs.edu.cn>
2021-12-14 21:40:47 +0800

    first commit

---
+++ scripts/Mars_test.sh
@@ -0,0 +1,22 @@
+python Test.py  --arch 'STAM'\
+                --dataset 'mars'\
+                --model_spatial_pool 'avg'\
+                --model_temporal_pool 'avg'\
+                --train_sampler 'Begin_interval'\
+                --test_sampler 'dense'\
+                --transform_method 'consecutive'\
+                --sampler_method 'random'\
+                --triplet_distance 'cosine'\
+                --test_distance 'cosine'\
+                --is_cat 'yes'\
+                --feature_method 'cat'\
+                --is_mutual_channel_attention 'yes'\
+                --is_mutual_spatial_attention 'yes'\
+                --is_appearance_channel_attention 'yes'\
+                --is_appearance_spatial_attention 'yes'\
+                --layer_num 3 \
+                --seq_len 8 \
+                --is_down_channel 'no'\
+                --print_performance True\
+                --layer_name 'down_channel'\
+                --test_path '/home/wyq/my_data/Model_pth/PSTA/Mars/Reference_Two_Mars/rank1_0.90656567_checkpoint_ep450.pth'
